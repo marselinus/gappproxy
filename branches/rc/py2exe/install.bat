51d8d92badcfb68ac3f5994ebf36df1e7670ddfe
lovelywcm <lovelywcm@537ec0d7-5e4f-0410-aa54-27559c288bec>
2008-10-15 11:10:04 +0000

    install.bat: 修正xp下的一个小问题

--- branches/rc/py2exe/install.bat
+++ branches/rc/py2exe/install.bat
@@ -1,9 +1,9 @@
 @echo sc stop GAppProxy > Register_As_Server.bat
 @echo sc delete GAppProxy >> Register_As_Server.bat
-@echo sc create GAppProxy binPath= %cd%\srvany.exe start= auto >> Register_As_Server.bat
+@echo sc create GAppProxy binPath= "%cd%\srvany.exe" start= auto >> Register_As_Server.bat
 @echo sc description GAppProxy "HTTP 代理服务 - GAppProxy 为您效劳。" >> Register_As_Server.bat
-@echo reg add HKLM\SYSTEM\CurrentControlSet\Services\GAppProxy\Parameters /v Application /d %cd%\proxy.exe >> Register_As_Server.bat /f
-@echo reg add HKLM\SYSTEM\CurrentControlSet\Services\GAppProxy\Parameters /v AppDirectory /d %cd% >> Register_As_Server.bat /f
+@echo reg add HKLM\SYSTEM\CurrentControlSet\Services\GAppProxy\Parameters /v Application /d "%cd%\proxy.exe" >> Register_As_Server.bat /f
+@echo reg add HKLM\SYSTEM\CurrentControlSet\Services\GAppProxy\Parameters /v AppDirectory /d "%cd%" >> Register_As_Server.bat /f
 @echo sc start GAppProxy >> Register_As_Server.bat
 
 @echo sc stop GAppProxy > Delete_GAppProxy_Server.bat
